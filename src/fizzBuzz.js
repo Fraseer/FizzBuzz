e36dd522bbc2eb9a8bd7b6b36ed13b80b1ac1444
Fraser <Fraser.Hughes@btinternet.com>
2021-07-28 18:08:00 +0100

    "Created tests for 1, 3, 5, & 15 on both Ruby & JS."

---
+++ src/fizzBuzz.js
@@ -0,0 +1,15 @@
+class FizzBuzz {
+  play(number) {
+    if (number % 3 == 0 && number % 5 == 0) {
+      return "FizzBuzz";
+    } else if (number % 3 == 0) {
+      return "fizz";
+    } else if (number % 5 == 0) {
+      return "buzz";
+    } else {
+      return number;
+    }
+  }
+}
+
+module.exports = FizzBuzz;
